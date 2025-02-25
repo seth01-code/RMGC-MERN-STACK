7e6c4d78474efa516edba7c850bd026379945ada
seth01-code <sethikhielea@gmail.com>
2025-02-25 13:53:29 +0100

    stuff

--- api/routes/UserRoute.js
+++ api/routes/UserRoute.js
@@ -15,18 +15,18 @@ import { verifyAdmin } from "../middleware/verifyAdmin.js";
 const router = express.Router();
 
 router.get("/me", getUserData);
-router.get("/profile", verifyToken, verifySeller, getUserProfile);
-router.get("/",  getUsers); // Fetch all users route
+router.get("/profile", getUserProfile);
+router.get("/", getUsers); // Fetch all users route
 router.get("/sellers", getSellers); // Fetch all Sellers route
-router.get("/:id", verifyToken, getUser);
-router.delete("/:id", verifyToken, verifyAdmin, deleteUser);
+router.get("/:id", getUser);
+router.delete("/:id", deleteUser);
 
 // Get user profile info
 
 // Update user profile info
-router.patch("/profile", verifyToken, verifySeller, updateUser);
+router.patch("/profile", updateUser);
 
 // Get total revenue (earnings) for the seller
-router.get("/revenue", verifySeller, getTotalRevenue);
+router.get("/revenue", getTotalRevenue);
 
 export default router;
