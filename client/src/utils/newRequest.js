ff5d0cd506781485bc503cdf6fb1e4e558881920
seth01-code <sethikhielea@gmail.com>
2025-02-27 06:47:28 +0100

    omo

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://rmgc-mern-stack-6.onrender.com/api",  // Make sure this is correct
-  withCredentials: true,  // To send cookies along with the request
+  baseURL: "https://api.renewedmindsglobalconsult.com/api", // Make sure this is correct
+  withCredentials: true, // To send cookies along with the request
 });
 
 export default newRequest;
