f811c923f34ba1c530578697bd58bd5e3b83c973
seth01-code <sethikhielea@gmail.com>
2025-02-27 15:00:14 +0100

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0, // Forces images to be separate files
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    headers: {
+      "Cache-Control": "public, max-age=31536000, immutable",
+    },
+  },
+});
