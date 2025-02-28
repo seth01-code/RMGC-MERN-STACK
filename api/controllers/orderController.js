6c9b6d6d947c414c790d55e991b5b1ec6fa3368a
seth01-code <sethikhielea@gmail.com>
2025-02-28 08:35:20 +0100

    com

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -88,7 +88,7 @@ export const intent = async (req, res, next) => {
       },
       {
         headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`,
+          Authorization: `Bearer ${process.env.PAYSTACK_LIVE_SECRET_KEY}`,
         },
       }
     );
@@ -105,7 +105,7 @@ export const intent = async (req, res, next) => {
 
 export const paystackWebhook = async (req, res, next) => {
   try {
-    const paystackSecret = process.env.PAYSTACK_TEST_SECRET_KEY;
+    const paystackSecret = process.env.PAYSTACK_LIVE_SECRET_KEY;
     const hash = crypto
       .createHmac("sha512", paystackSecret)
       .update(JSON.stringify(req.body))
