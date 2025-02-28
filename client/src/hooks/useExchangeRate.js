97060a2903f9aaf64e95efd351059486281989a8
seth01-code <sethikhielea@gmail.com>
2025-02-28 10:37:42 +0100

    comit

--- client/src/hooks/useExchangeRate.js
+++ client/src/hooks/useExchangeRate.js
@@ -23,7 +23,7 @@ export const useExchangeRate = (country) => {
     const fetchExchangeRate = async () => {
       try {
         const res = await axios.get(
-          `https://open.er-api.com/v6/latest/${BASE_CURRENCY}`
+          `https://api.exchangerate-api.com/v4/latest/${BASE_CURRENCY}`
         );
 
         if (res.data.rates && res.data.rates[countryCurrency]) {
