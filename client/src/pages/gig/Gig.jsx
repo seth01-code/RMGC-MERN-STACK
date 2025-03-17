3f91f033a92ae5e0d3c6764f3697d1eb7e60a1a4
seth01-code <sethikhielea@gmail.com>
2025-03-17 01:07:14 +0100

    height

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -246,7 +246,7 @@ const Gig = () => {
           </div>
 
           {/* RIGHT SECTION */}
-          <div className="flex-1 border h-3/5 border-gray-300 p-5 rounded-md shadow-md lg:sticky top-36 min-w-[280px]">
+          <div className="border border-gray-300 p-5 rounded-md shadow-md lg:sticky top-36 min-w-[280px] w-full max-h-fit">
             <div className="flex items-center justify-between">
               <h3 className="text-md md:text-lg font-medium">
                 {data.shortTitle}
