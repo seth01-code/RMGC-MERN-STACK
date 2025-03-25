073f99c8a1180600d7aed8822203678a8851d51d
seth01-code <sethikhielea@gmail.com>
2025-03-25 23:23:45 +0100

    ads

--- client/src/components/GoogleAd.jsx
+++ client/src/components/GoogleAd.jsx
@@ -22,14 +22,16 @@ const GoogleAd = () => {
   }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-8713973295876975"
-      data-ad-slot="5074558232"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
+    <div className="flex items-center justify-center min-h-screen bg-[#f2f2f2]">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-8713973295876975"
+        data-ad-slot="5074558232"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
   );
 };
 
