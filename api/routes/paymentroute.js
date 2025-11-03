54f1692c192735879e1c136a00d38bd7373ae041
Prideof_All <sethikhielea@gmail.com>
2025-11-03 18:08:02 +0100

    payment

--- api/routes/paymentroute.js
+++ api/routes/paymentroute.js
@@ -1,9 +1,5 @@
 import express from "express";
-import {
-  createOrganizationSubscription,
-  validateOrganizationOtp,
-  verifyOrganizationPayment,
-} from "../controllers/organizationPaymentController.js";
+import { createOrganizationSubscription, verifyOrganizationPayment } from "../controllers/organizationPaymentController.js";
 import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
@@ -13,7 +9,7 @@ router.post(
   verifyToken,
   createOrganizationSubscription
 );
-router.post("/organization/validate-otp", verifyToken, validateOrganizationOtp);
+
 router.post("/organization/verify", verifyToken, verifyOrganizationPayment);
 
 export default router;
